3f638feea9dbb72a9595c19ce945b3b73473f092
Jordan Russell <jwrussell83@gmail.com>
2017-02-21 23:29:43 -0800

    started user list and user page section

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
   get 'pages/home'
-  root "pins#index"
-  
+  root 'pins#index'
+
+  get 'users' => 'users#index'
+  get 'users/:id' => 'users#show'
+
   get 'about' => 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
